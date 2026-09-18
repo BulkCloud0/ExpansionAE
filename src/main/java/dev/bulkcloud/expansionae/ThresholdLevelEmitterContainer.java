c3b6c6a36c0556ff7eb59ef11a87ec96003cab40
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-18 17:43:32 -0300

    Add threshold level emitter UI

--- src/main/java/dev/bulkcloud/expansionae/ThresholdLevelEmitterContainer.java
+++ src/main/java/dev/bulkcloud/expansionae/ThresholdLevelEmitterContainer.java
@@ -1,9 +1,11 @@
 package dev.bulkcloud.expansionae;
 
 import appeng.api.config.FuzzyMode;
+import appeng.api.config.LevelType;
 import appeng.api.config.RedstoneMode;
 import appeng.api.config.SecurityPermissions;
 import appeng.api.config.Settings;
+import appeng.api.config.YesNo;
 import appeng.container.SlotSemantic;
 import appeng.container.guisync.GuiSync;
 import appeng.container.implementations.ContainerTypeBuilder;
@@ -13,23 +15,35 @@ import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.container.ContainerType;
 import net.minecraftforge.items.IItemHandler;
 
-public final class ThresholdLevelEmitterContainer extends UpgradeableContainer implements TextFilterReceiver {
+public final class ThresholdLevelEmitterContainer extends UpgradeableContainer {
     public static final ContainerType<ThresholdLevelEmitterContainer> TYPE = ContainerTypeBuilder
             .create(ThresholdLevelEmitterContainer::new, ThresholdLevelEmitter.class)
             .requirePermission(SecurityPermissions.BUILD)
+            .withInitialData((host, buffer) -> {
+                buffer.writeVarLong(host.getUpperValue());
+                buffer.writeVarLong(host.getLowerValue());
+            }, (host, container, buffer) -> {
+                container.upperValue = buffer.readVarLong();
+                container.lowerValue = buffer.readVarLong();
+            })
             .build("expansionae_threshold_level_emitter");
 
     private final ThresholdLevelEmitter host;
 
-    @GuiSync(21)
+    @GuiSync(7)
     public long upperValue;
-
-    @GuiSync(22)
+    @GuiSync(8)
     public long lowerValue;
+    @GuiSync(9)
+    public LevelType levelType;
+    @GuiSync(10)
+    public YesNo craftingMode;
 
     public ThresholdLevelEmitterContainer(int id, PlayerInventory player, ThresholdLevelEmitter host) {
         super(TYPE, id, player, host);
         this.host = host;
+        registerClientAction("setUpperValue", Long.class, this::setUpperValue);
+        registerClientAction("setLowerValue", Long.class, this::setLowerValue);
     }
 
     @Override
@@ -49,25 +63,57 @@ public final class ThresholdLevelEmitterContainer extends UpgradeableContainer i
         return 1;
     }
 
+    public void setUpperValue(long value) {
+        value = Math.max(0, value);
+        if (isRemote()) {
+            if (value != upperValue) {
+                upperValue = value;
+                sendClientAction("setUpperValue", value);
+            }
+        } else {
+            host.setUpperValue(value);
+            upperValue = host.getUpperValue();
+        }
+    }
+
+    public void setLowerValue(long value) {
+        value = Math.max(0, value);
+        if (isRemote()) {
+            if (value != lowerValue) {
+                lowerValue = value;
+                sendClientAction("setLowerValue", value);
+            }
+        } else {
+            host.setLowerValue(value);
+            lowerValue = host.getLowerValue();
+        }
+    }
+
     @Override
     public void detectAndSendChanges() {
         verifyPermissions(SecurityPermissions.BUILD, false);
         if (isServer()) {
-            setFuzzyMode((FuzzyMode) host.getConfigManager().getSetting(Settings.FUZZY_MODE));
-            setRedStoneMode((RedstoneMode) host.getConfigManager().getSetting(Settings.REDSTONE_EMITTER));
             upperValue = host.getUpperValue();
             lowerValue = host.getLowerValue();
+            levelType = (LevelType) host.getConfigManager().getSetting(Settings.LEVEL_TYPE);
+            craftingMode = (YesNo) host.getConfigManager().getSetting(Settings.CRAFT_VIA_REDSTONE);
+            setFuzzyMode((FuzzyMode) host.getConfigManager().getSetting(Settings.FUZZY_MODE));
+            setRedStoneMode((RedstoneMode) host.getConfigManager().getSetting(Settings.REDSTONE_EMITTER));
         }
         standardDetectAndSendChanges();
     }
 
+    public LevelType getLevelType() {
+        return levelType;
+    }
+
     @Override
-    public void applyTextFilter(int key, String value) {
-        try {
-            long parsed = Math.max(0L, Long.parseLong(value.trim()));
-            if (key == 0) host.setUpperValue(parsed);
-            if (key == 1) host.setLowerValue(parsed);
-        } catch (NumberFormatException ignored) {
-        }
+    public YesNo getCraftingMode() {
+        return craftingMode;
+    }
+
+    @Override
+    public void setCraftingMode(YesNo mode) {
+        craftingMode = mode;
     }
 }
--- src/main/java/dev/bulkcloud/expansionae/client/ThresholdLevelEmitterScreen.java
+++ src/main/java/dev/bulkcloud/expansionae/client/ThresholdLevelEmitterScreen.java
@@ -1,74 +1,97 @@
 package dev.bulkcloud.expansionae.client;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
+
 import appeng.api.config.FuzzyMode;
+import appeng.api.config.LevelType;
 import appeng.api.config.RedstoneMode;
 import appeng.api.config.Settings;
 import appeng.api.config.Upgrades;
+import appeng.api.config.YesNo;
+import appeng.client.gui.NumberEntryType;
+import appeng.client.gui.implementations.NumberEntryWidget;
 import appeng.client.gui.implementations.UpgradeableScreen;
+import appeng.client.gui.style.PaletteColor;
 import appeng.client.gui.style.ScreenStyle;
-import appeng.client.gui.widgets.AETextField;
 import appeng.client.gui.widgets.ServerSettingToggleButton;
 import appeng.client.gui.widgets.SettingToggleButton;
-import dev.bulkcloud.expansionae.ExpansionNetwork;
-import dev.bulkcloud.expansionae.TextFilterUpdate;
 import dev.bulkcloud.expansionae.ThresholdLevelEmitterContainer;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 
 public final class ThresholdLevelEmitterScreen extends UpgradeableScreen<ThresholdLevelEmitterContainer> {
-    private final SettingToggleButton<RedstoneMode> redstone;
-    private final SettingToggleButton<FuzzyMode> fuzzy;
-    private AETextField upper;
-    private AETextField lower;
+    private final SettingToggleButton<LevelType> levelMode;
+    private final SettingToggleButton<YesNo> craftingMode;
+    private final SettingToggleButton<RedstoneMode> redstoneMode;
+    private final SettingToggleButton<FuzzyMode> fuzzyMode;
+    private final NumberEntryWidget upper;
+    private final NumberEntryWidget lower;
 
     public ThresholdLevelEmitterScreen(ThresholdLevelEmitterContainer container, PlayerInventory player,
             ITextComponent title, ScreenStyle style) {
         super(container, player, title, style);
-        redstone = new ServerSettingToggleButton<>(Settings.REDSTONE_EMITTER, RedstoneMode.HIGH_SIGNAL);
-        fuzzy = new ServerSettingToggleButton<>(Settings.FUZZY_MODE, FuzzyMode.IGNORE_ALL);
-        addToLeftToolbar(redstone);
-        addToLeftToolbar(fuzzy);
+        levelMode = new ServerSettingToggleButton<>(Settings.LEVEL_TYPE, LevelType.ITEM_LEVEL);
+        craftingMode = new ServerSettingToggleButton<>(Settings.CRAFT_VIA_REDSTONE, YesNo.NO);
+        redstoneMode = new ServerSettingToggleButton<>(Settings.REDSTONE_EMITTER, RedstoneMode.HIGH_SIGNAL);
+        fuzzyMode = new ServerSettingToggleButton<>(Settings.FUZZY_MODE, FuzzyMode.IGNORE_ALL);
+        addToLeftToolbar(levelMode);
+        addToLeftToolbar(redstoneMode);
+        addToLeftToolbar(craftingMode);
+        addToLeftToolbar(fuzzyMode);
+
+        upper = new NumberEntryWidget(NumberEntryType.LEVEL_ITEM_COUNT);
+        upper.setTextFieldBounds(86, 39, 76);
+        upper.setValue(container.upperValue);
+        upper.setOnChange(this::saveUpper);
+        widgets.add("upper", upper);
+
+        lower = new NumberEntryWidget(NumberEntryType.LEVEL_ITEM_COUNT);
+        lower.setTextFieldBounds(86, 62, 76);
+        lower.setValue(container.lowerValue);
+        lower.setOnChange(this::saveLower);
+        lower.setOnConfirm(this::closeScreen);
+        widgets.add("lower", lower);
     }
 
-    @Override
-    protected void init() {
-        super.init();
-
-        upper = new AETextField(font, guiLeft + 93, guiTop + 30, 72, 14);
-        upper.setMaxStringLength(19);
-        upper.setText(Long.toString(container.upperValue));
-        upper.setResponder(value -> send(0, value));
-        addListener(upper);
-
-        lower = new AETextField(font, guiLeft + 93, guiTop + 52, 72, 14);
-        lower.setMaxStringLength(19);
-        lower.setText(Long.toString(container.lowerValue));
-        lower.setResponder(value -> send(1, value));
-        addListener(lower);
-
-        setListenerDefault(upper);
+    private void saveUpper() {
+        upper.getLongValue().ifPresent(container::setUpperValue);
     }
 
-    private void send(int key, String value) {
-        if (value != null && !value.trim().isEmpty()) {
-            ExpansionNetwork.sendToServer(new TextFilterUpdate(container.windowId, key, value));
-        }
+    private void saveLower() {
+        lower.getLongValue().ifPresent(container::setLowerValue);
     }
 
     @Override
     protected void updateBeforeRender() {
         super.updateBeforeRender();
-        redstone.set(container.getRedStoneMode());
-        fuzzy.set(container.getFuzzyMode());
-        fuzzy.setVisibility(container.hasUpgrade(Upgrades.FUZZY));
+        boolean notCrafting = !container.hasUpgrade(Upgrades.CRAFTING);
+        upper.setActive(notCrafting);
+        lower.setActive(notCrafting);
+        levelMode.active = notCrafting;
+        levelMode.set(container.getLevelType());
+        redstoneMode.active = notCrafting;
+        redstoneMode.set(container.getRedStoneMode());
+        craftingMode.set(container.getCraftingMode());
+        craftingMode.setVisibility(!notCrafting);
+        fuzzyMode.set(container.getFuzzyMode());
+        fuzzyMode.setVisibility(container.hasUpgrade(Upgrades.FUZZY));
+    }
 
-        if (upper != null && !upper.isFocused()) {
-            String value = Long.toString(container.upperValue);
-            if (!upper.getText().equals(value)) upper.setText(value);
-        }
-        if (lower != null && !lower.isFocused()) {
-            String value = Long.toString(container.lowerValue);
-            if (!lower.getText().equals(value)) lower.setText(value);
-        }
+    @Override
+    public void drawFG(MatrixStack matrices, int offsetX, int offsetY, int mouseX, int mouseY) {
+        super.drawFG(matrices, offsetX, offsetY, mouseX, mouseY);
+        int color = style.getColor(PaletteColor.DEFAULT_TEXT_COLOR).toARGB();
+        font.drawString(matrices, new TranslationTextComponent("gui.expansionae.threshold_level.upper").getString(),
+                28, 43, color);
+        font.drawString(matrices, new TranslationTextComponent("gui.expansionae.threshold_level.lower").getString(),
+                28, 66, color);
+    }
+
+    @Override
+    public void drawBG(MatrixStack matrices, int offsetX, int offsetY, int mouseX, int mouseY, float partialTicks) {
+        super.drawBG(matrices, offsetX, offsetY, mouseX, mouseY, partialTicks);
+        upper.render(matrices, mouseX, mouseY, partialTicks);
+        lower.render(matrices, mouseX, mouseY, partialTicks);
     }
 }
