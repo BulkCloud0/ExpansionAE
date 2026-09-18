dbad582652a76b0bf2df1015f09138713cb811bc
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-18 17:21:08 -0300

    Add ExpandedAE drive block

---
+++ src/main/java/dev/bulkcloud/expansionae/ExpandedDriveBlock.java
@@ -0,0 +1,40 @@
+package dev.bulkcloud.expansionae;
+
+import javax.annotation.Nullable;
+
+import appeng.block.AEBaseTileBlock;
+import appeng.container.ContainerLocator;
+import appeng.container.ContainerOpener;
+import appeng.util.InteractionUtil;
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.world.World;
+
+public final class ExpandedDriveBlock extends AEBaseTileBlock<ExpandedDriveTile> {
+    public ExpandedDriveBlock() {
+        super(defaultProps(Material.IRON));
+    }
+
+    @Override
+    public ActionResultType onActivated(World world, BlockPos pos, PlayerEntity player, Hand hand,
+            @Nullable ItemStack heldItem, BlockRayTraceResult hit) {
+        if (InteractionUtil.isInAlternateUseMode(player)) {
+            return ActionResultType.PASS;
+        }
+
+        ExpandedDriveTile tile = getTileEntity(world, pos);
+        if (tile != null) {
+            if (!world.isRemote()) {
+                ContainerOpener.openContainer(ExpandedDriveContainer.TYPE, player,
+                        ContainerLocator.forTileEntity(tile));
+            }
+            return ActionResultType.func_233537_a_(world.isRemote());
+        }
+        return ActionResultType.PASS;
+    }
+}
