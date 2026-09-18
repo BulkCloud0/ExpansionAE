8ed0100c56fac3668498c2510841d3f5828bf1ab
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-18 17:41:48 -0300

    Register threshold level emitter screen

--- src/main/java/dev/bulkcloud/expansionae/client/ClientSetup.java
+++ src/main/java/dev/bulkcloud/expansionae/client/ClientSetup.java
@@ -11,6 +11,7 @@ import dev.bulkcloud.expansionae.TagExportBusContainer;
 import dev.bulkcloud.expansionae.ModStorageBusContainer;
 import dev.bulkcloud.expansionae.TagStorageBusContainer;
 import dev.bulkcloud.expansionae.PreciseStorageBusContainer;
+import dev.bulkcloud.expansionae.ThresholdLevelEmitterContainer;
 import dev.bulkcloud.expansionae.ExpandedContainer;
 import dev.bulkcloud.expansionae.ExpandedDriveContainer;
 import dev.bulkcloud.expansionae.PatternEncoderContainer;
@@ -89,6 +90,11 @@ public final class ClientSetup {
             try {
                 return new PreciseStorageBusScreen(container, inventory, title, StyleManager.loadStyleDoc("/screens/storage_bus.json"));
             } catch (IOException e) { throw new IllegalStateException("Cannot load precise storage bus screen", e); }
+        }));
+        event.enqueueWork(() -> ScreenManager.<ThresholdLevelEmitterContainer, ThresholdLevelEmitterScreen>registerFactory(ThresholdLevelEmitterContainer.TYPE, (container, inventory, title) -> {
+            try {
+                return new ThresholdLevelEmitterScreen(container, inventory, title, StyleManager.loadStyleDoc("/screens/level_emitter.json"));
+            } catch (IOException e) { throw new IllegalStateException("Cannot load threshold level emitter screen", e); }
         }));
         event.enqueueWork(() -> ScreenManager.<ExpandedDriveContainer, ExpandedDriveScreen>registerFactory(ExpandedDriveContainer.TYPE, (container, inventory, title) -> {
             try {
