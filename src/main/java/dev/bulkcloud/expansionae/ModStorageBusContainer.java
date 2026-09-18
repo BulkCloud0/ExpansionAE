866549765c874a0cfb49a6568a67d973d2bf6886
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-18 17:34:53 -0300

    Sync filtered storage bus settings

--- src/main/java/dev/bulkcloud/expansionae/ModStorageBusContainer.java
+++ src/main/java/dev/bulkcloud/expansionae/ModStorageBusContainer.java
@@ -1,6 +1,9 @@
 package dev.bulkcloud.expansionae;
 
+import appeng.api.config.AccessRestriction;
 import appeng.api.config.SecurityPermissions;
+import appeng.api.config.Settings;
+import appeng.api.config.StorageFilter;
 import appeng.container.guisync.GuiSync;
 import appeng.container.implementations.ContainerTypeBuilder;
 import appeng.container.implementations.UpgradeableContainer;
@@ -18,6 +21,12 @@ public final class ModStorageBusContainer extends UpgradeableContainer implement
     @GuiSync(13)
     public String filter = "";
 
+    @GuiSync(16)
+    public AccessRestriction access = AccessRestriction.READ_WRITE;
+
+    @GuiSync(17)
+    public StorageFilter storageFilter = StorageFilter.EXTRACTABLE_ONLY;
+
     public ModStorageBusContainer(int id, PlayerInventory player, ModStorageBus host) {
         super(TYPE, id, player, host);
         this.host = host;
@@ -28,9 +37,21 @@ public final class ModStorageBusContainer extends UpgradeableContainer implement
         setupUpgrades();
     }
 
+    public AccessRestriction getReadWriteMode() {
+        return access;
+    }
+
+    public StorageFilter getStorageFilter() {
+        return storageFilter;
+    }
+
     @Override
     public void detectAndSendChanges() {
-        if (isServer()) filter = host.getModFilter();
+        if (isServer()) {
+            filter = host.getModFilter();
+            access = (AccessRestriction) host.getConfigManager().getSetting(Settings.ACCESS);
+            storageFilter = (StorageFilter) host.getConfigManager().getSetting(Settings.STORAGE_FILTER);
+        }
         super.detectAndSendChanges();
     }
 
