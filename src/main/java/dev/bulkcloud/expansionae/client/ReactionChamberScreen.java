01517c30273a96a09e89036d205cb09dc8797b93
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-21 11:28:43 -0300

    Fix Reaction Chamber direction type conflict

--- src/main/java/dev/bulkcloud/expansionae/client/ReactionChamberScreen.java
+++ src/main/java/dev/bulkcloud/expansionae/client/ReactionChamberScreen.java
@@ -8,12 +8,11 @@ import dev.bulkcloud.expansionae.ReactionChamberContainer;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.Direction;
 import net.minecraft.client.gui.widget.button.Button;
 
 public final class ReactionChamberScreen extends UpgradeableScreen<ReactionChamberContainer> {
     private final ProgressBar progress;
-    private final Button[] outputButtons = new Button[Direction.values().length];
+    private final Button[] outputButtons = new Button[net.minecraft.util.Direction.values().length];
 
     public ReactionChamberScreen(ReactionChamberContainer container, PlayerInventory player,
             ITextComponent title, ScreenStyle style) {
@@ -25,9 +24,9 @@ public final class ReactionChamberScreen extends UpgradeableScreen<ReactionChamb
     @Override
     protected void init() {
         super.init();
-        Direction[] directions = Direction.values();
+        net.minecraft.util.Direction[] directions = net.minecraft.util.Direction.values();
         for (int i = 0; i < directions.length; i++) {
-            final Direction direction = directions[i];
+            final net.minecraft.util.Direction direction = directions[i];
             int x = guiLeft + 8 + (i % 3) * 28;
             int y = guiTop + 18 + (i / 3) * 18;
             outputButtons[i] = addButton(new Button(x, y, 26, 16,
@@ -35,7 +34,7 @@ public final class ReactionChamberScreen extends UpgradeableScreen<ReactionChamb
         }
     }
 
-    private ITextComponent outputLabel(Direction direction) {
+    private ITextComponent outputLabel(net.minecraft.util.Direction direction) {
         String id = direction.getString().substring(0, 1).toUpperCase();
         return new StringTextComponent((container.isOutputEnabled(direction) ? "*" : "-") + id);
     }
@@ -45,7 +44,7 @@ public final class ReactionChamberScreen extends UpgradeableScreen<ReactionChamb
         super.updateBeforeRender();
         int pct = container.getCurrentProgress() * 100 / Math.max(1, container.getMaxProgress());
         progress.setFullMsg(new StringTextComponent(pct + "%"));
-        for (Direction direction : Direction.values()) {
+        for (net.minecraft.util.Direction direction : net.minecraft.util.Direction.values()) {
             Button button = outputButtons[direction.getIndex()];
             if (button != null) button.setMessage(outputLabel(direction));
         }
