d8c47a974357c8f60cd417298bd3e4fb29fc53a3
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-22 11:18:44 -0300

    feat(disk): register 1k disk item

--- src/main/java/com/bulkcloud/expansionae/core/registry/ExpansionAEItems.java
+++ src/main/java/com/bulkcloud/expansionae/core/registry/ExpansionAEItems.java
@@ -4,12 +4,18 @@ import net.minecraft.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.fml.RegistryObject;
 
 import com.bulkcloud.expansionae.ExpansionAE;
+import com.bulkcloud.expansionae.feature.disk.DiskStorageCellItem;
 
 public final class ExpansionAEItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, ExpansionAE.MOD_ID);
+
+    public static final RegistryObject<Item> DISK_1K = ITEMS.register(
+            "1k_disk",
+            () -> new DiskStorageCellItem(new Item.Properties(), 1000, 0.5));
 
     private ExpansionAEItems() {
     }
