627d31eb2b4d47d2a83f1d41fd97e6905f779a47
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-22 16:51:56 -0300

    feat: register Boosted Growth tile entity

--- src/main/java/com/bulkcloud/expansionae/core/registry/ExpansionAETileEntities.java
+++ src/main/java/com/bulkcloud/expansionae/core/registry/ExpansionAETileEntities.java
@@ -7,6 +7,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import com.bulkcloud.expansionae.ExpansionAE;
+import com.bulkcloud.expansionae.feature.growth.BoostedGrowthAcceleratorTileEntity;
 import com.bulkcloud.expansionae.feature.growth.CrankedGrowthAcceleratorTileEntity;
 
 public final class ExpansionAETileEntities {
@@ -19,6 +20,14 @@ public final class ExpansionAETileEntities {
                     () -> TileEntityType.Builder.create(
                             CrankedGrowthAcceleratorTileEntity::new,
                             ExpansionAEBlocks.CRANKED_GROWTH_ACCELERATOR.get())
+                            .build(null));
+
+    public static final RegistryObject<TileEntityType<BoostedGrowthAcceleratorTileEntity>>
+            BOOSTED_GROWTH_ACCELERATOR = TILE_ENTITIES.register(
+                    "boosted_growth_accelerator",
+                    () -> TileEntityType.Builder.create(
+                            BoostedGrowthAcceleratorTileEntity::new,
+                            ExpansionAEBlocks.BOOSTED_GROWTH_ACCELERATOR.get())
                             .build(null));
 
     private ExpansionAETileEntities() {
