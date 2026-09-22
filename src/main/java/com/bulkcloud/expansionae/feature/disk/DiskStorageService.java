77221aed115e458bc03a3b1b3f90a74790a89a5f
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-22 14:58:28 -0300

    fix: use MCP world dimension accessor

--- src/main/java/com/bulkcloud/expansionae/feature/disk/DiskStorageService.java
+++ src/main/java/com/bulkcloud/expansionae/feature/disk/DiskStorageService.java
@@ -19,7 +19,7 @@ public final class DiskStorageService {
         }
 
         ServerWorld world = (ServerWorld) event.getWorld();
-        if (world.dimension() == World.OVERWORLD) {
+        if (world.getDimensionKey().equals(World.OVERWORLD)) {
             current = DiskStorageData.get(world);
         }
     }
@@ -30,7 +30,7 @@ public final class DiskStorageService {
         }
 
         ServerWorld world = (ServerWorld) event.getWorld();
-        if (world.dimension() == World.OVERWORLD) {
+        if (world.getDimensionKey().equals(World.OVERWORLD)) {
             current = null;
         }
     }
