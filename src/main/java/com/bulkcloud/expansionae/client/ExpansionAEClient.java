43db13c1da4585e010e232d987d560c3caf6e0ed
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-22 17:10:10 -0300

    fix: queue DISK drive models for client loading

--- src/main/java/com/bulkcloud/expansionae/client/ExpansionAEClient.java
+++ src/main/java/com/bulkcloud/expansionae/client/ExpansionAEClient.java
@@ -1,8 +1,10 @@
 package com.bulkcloud.expansionae.client;
 
+import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -24,19 +26,40 @@ public final class ExpansionAEClient {
     public static void onModelRegistry(ModelRegistryEvent event) {
         ICellModelRegistry cells = ExpansionAEApi.get().client().cells();
 
-        cells.registerModel(
+        registerDiskModel(
+                cells,
                 ExpansionAEItems.DISK_1K.get(),
                 new ResourceLocation("appliedenergistics2:block/drive/cells/1k_item_cell"));
-        cells.registerModel(
+        registerDiskModel(
+                cells,
                 ExpansionAEItems.DISK_4K.get(),
                 new ResourceLocation("appliedenergistics2:block/drive/cells/4k_item_cell"));
-        cells.registerModel(
+        registerDiskModel(
+                cells,
                 ExpansionAEItems.DISK_16K.get(),
                 new ResourceLocation("appliedenergistics2:block/drive/cells/16k_item_cell"));
-        cells.registerModel(
+        registerDiskModel(
+                cells,
                 ExpansionAEItems.DISK_64K.get(),
                 new ResourceLocation("appliedenergistics2:block/drive/cells/64k_item_cell"));
 
-        ExpansionAE.LOGGER.info("Registered 1k/4k/16k/64k DISK models with the AE2 client cell registry");
+        ExpansionAE.LOGGER.info(
+                "Registered and queued 1k/4k/16k/64k DISK drive models with the AE2 client cell registry");
+    }
+
+    private static void registerDiskModel(
+            ICellModelRegistry cells,
+            Item item,
+            ResourceLocation model) {
+        // AE2 8.4.x explicitly requires addon cell models to be queued for loading.
+        // Registering only the item -> model mapping is not sufficient by API contract.
+        ModelLoader.addSpecialModel(model);
+        cells.registerModel(item, model);
+
+        if (!model.equals(cells.model(item))) {
+            throw new IllegalStateException(
+                    "AE2 client cell model registry did not retain model " + model
+                            + " for " + item.getRegistryName());
+        }
     }
 }
