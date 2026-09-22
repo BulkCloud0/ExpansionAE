897227b547e74bd90dbd8f2e5ef8cedf1d83f4c1
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-22 16:20:47 -0300

    feat: register cranked growth accelerator block

--- src/main/java/com/bulkcloud/expansionae/core/registry/ExpansionAEBlocks.java
+++ src/main/java/com/bulkcloud/expansionae/core/registry/ExpansionAEBlocks.java
@@ -4,12 +4,17 @@ import net.minecraft.block.Block;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.fml.RegistryObject;
 
 import com.bulkcloud.expansionae.ExpansionAE;
+import com.bulkcloud.expansionae.feature.growth.CrankedGrowthAcceleratorBlock;
 
 public final class ExpansionAEBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, ExpansionAE.MOD_ID);
+
+    public static final RegistryObject<Block> CRANKED_GROWTH_ACCELERATOR =
+            BLOCKS.register("cranked_growth_accelerator", CrankedGrowthAcceleratorBlock::new);
 
     private ExpansionAEBlocks() {
     }
