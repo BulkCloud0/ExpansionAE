184a4e4067b1653db69e81b4999800e2a9796ece
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-22 11:18:48 -0300

    feat(disk): initialize disk storage integration

--- src/main/java/com/bulkcloud/expansionae/ExpansionAE.java
+++ src/main/java/com/bulkcloud/expansionae/ExpansionAE.java
@@ -6,10 +6,17 @@ import org.apache.logging.log4j.Logger;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.event.world.WorldEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import com.bulkcloud.expansionae.core.registry.ExpansionAEBlocks;
 import com.bulkcloud.expansionae.core.registry.ExpansionAEItems;
+import com.bulkcloud.expansionae.feature.disk.DiskCellHandler;
+import com.bulkcloud.expansionae.feature.disk.DiskStorageService;
+
+import appeng.core.Api;
 
 @Mod(ExpansionAE.MOD_ID)
 public final class ExpansionAE {
@@ -21,8 +28,24 @@ public final class ExpansionAE {
 
         ExpansionAEBlocks.register(modBus);
         ExpansionAEItems.register(modBus);
+        modBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
         LOGGER.info("ExpansionAE bootstrap initialized");
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+        event.enqueueWork(() ->
+                Api.instance().registries().cell().addCellHandler(DiskCellHandler.INSTANCE));
+    }
+
+    @SubscribeEvent
+    public void onWorldLoad(WorldEvent.Load event) {
+        DiskStorageService.onWorldLoad(event);
+    }
+
+    @SubscribeEvent
+    public void onWorldUnload(WorldEvent.Unload event) {
+        DiskStorageService.onWorldUnload(event);
     }
 }
