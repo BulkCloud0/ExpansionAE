7679eed5ad653f8f70d720513249022789af2e1e
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-24 10:49:00 -0300

    develop: unify current ExpansionAE feature train

---
+++ src/main/java/com/bulkcloud/expansionae/core/registry/ExpansionAEContainers.java
@@ -0,0 +1,28 @@
+package com.bulkcloud.expansionae.core.registry;
+
+import net.minecraft.inventory.container.ContainerType;
+import net.minecraftforge.common.extensions.IForgeContainerType;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import com.bulkcloud.expansionae.ExpansionAE;
+import com.bulkcloud.expansionae.feature.stockexport.StockExportBusContainer;
+
+public final class ExpansionAEContainers {
+    public static final DeferredRegister<ContainerType<?>> CONTAINERS =
+            DeferredRegister.create(ForgeRegistries.CONTAINERS, ExpansionAE.MOD_ID);
+
+    public static final RegistryObject<ContainerType<StockExportBusContainer>> STOCK_EXPORT_BUS =
+            CONTAINERS.register(
+                    "stock_export_bus",
+                    () -> IForgeContainerType.create(StockExportBusContainer::fromNetwork));
+
+    private ExpansionAEContainers() {
+    }
+
+    public static void register(IEventBus modBus) {
+        CONTAINERS.register(modBus);
+    }
+}
