3ffd2bf611ed0bbfe0edad19ed125e376bf294b3
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-24 11:59:31 -0300

    fix: type boosted accelerator block to its tile

--- src/main/java/com/bulkcloud/expansionae/feature/growth/BoostedGrowthAcceleratorBlock.java
+++ src/main/java/com/bulkcloud/expansionae/feature/growth/BoostedGrowthAcceleratorBlock.java
@@ -1,12 +1,38 @@
 package com.bulkcloud.expansionae.feature.growth;
 
-import appeng.block.misc.QuartzGrowthAcceleratorBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.StateContainer;
 
-public final class BoostedGrowthAcceleratorBlock extends QuartzGrowthAcceleratorBlock {
+import appeng.api.util.IOrientableBlock;
+import appeng.block.AEBaseTileBlock;
+
+public final class BoostedGrowthAcceleratorBlock
+        extends AEBaseTileBlock<BoostedGrowthAcceleratorTileEntity>
+        implements IOrientableBlock {
+    private static final BooleanProperty POWERED = BooleanProperty.create("powered");
+
     public BoostedGrowthAcceleratorBlock() {
-        super();
+        super(defaultProps(Material.ROCK).sound(SoundType.METAL));
+        this.setDefaultState(this.getDefaultState().with(POWERED, false));
         this.setTileEntity(
                 BoostedGrowthAcceleratorTileEntity.class,
                 BoostedGrowthAcceleratorTileEntity::new);
+    }
+
+    @Override
+    protected BlockState updateBlockStateFromTileEntity(
+            BlockState currentState,
+            BoostedGrowthAcceleratorTileEntity tile) {
+        return currentState.with(POWERED, tile.isPowered());
+    }
+
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        super.fillStateContainer(builder);
+        builder.add(POWERED);
     }
 }
